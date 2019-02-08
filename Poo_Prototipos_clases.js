47e906227b6025af5a9471214fbcf9b45b41a3f9
Luis Medina <lmedina@rapicredit.com>
2019-02-08 17:21:18 -0500

    avance del proyecto 20190208

--- Poo_Prototipos_clases.js
+++ Poo_Prototipos_clases.js
@@ -1,3 +1,29 @@
+class Persona {
+  constructor(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura  = altura
+  }
+
+  saludar(){
+      console.log(`Hola, Me llamo ${this.nombre} ${this.apellido}`)
+  }
+  soyAlto(){
+    return this.altura > 1.8
+  }
+}
+
+class Desarrollador extends Persona {
+  constructor(nombre, apellido, altura) {
+  super(nombre,apellido,altura)
+  }
+
+  saludar(){
+      console.log(`Hola, Me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
+  }
+
+}
+/*
 function Persona (nombre, apellido) {
   this.nombre = nombre
   this.apellido = apellido
@@ -7,6 +33,12 @@ Persona.prototype.saludar = function () {
 console.log(`Hola, Me llamo ${this.nombre} ${this.apellido}`)
 }
 
+Persona.prototype.soyAlto = function(){
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+}
+
+
 var Luis = new Persona('Luis', 'Medina')
 var Nidia = new Persona('Nidia', 'Gaviria')
 var None = new Persona('None', 'None')
+*/
